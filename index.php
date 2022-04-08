df96466094235266e56a06058c1b38071bc58d75
Jovo Nikolic <jnikolic.win-b2019@fh-salzburg.ac.at>
2022-04-08 19:41:29 +0200

    Create connection to database

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 echo "Hello World!";
-?>
+$host = 'testdatenbank.database.windows.net';
+$username = 'testAdmin';
+$password = 'Test123!';
+$db_name = 'testDatenbank';
+
+//Initializes MySQLi
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn, NULL, NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 1433, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno()) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
